789594140d201b90a3a05dc29f70412179fc8fdb
VictorPresenca <presenca.victor@gmail.com>
2025-12-06 22:21:07 -0300

    listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0:">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        teste
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        teste
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        teste
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        teste
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        teste
+        </div>
+      </div>
+
     </div>
   );
 }
