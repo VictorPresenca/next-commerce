d0be9f0da07ac1859224c99b4b92d3ac893b28d4
VictorPresenca <presenca.victor@gmail.com>
2025-12-06 22:40:45 -0300

    definindo tipagem para produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
+import { ProductType } from "@/types/ProductType";
+async function getProducts (){
+  const res = await fetch('https://fakestoreapi.com/products');
+  if (!res.ok){
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getProducts ();
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0:">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        teste
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        teste
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        teste
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        teste
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        teste
-        </div>
+        {products.map((products: ProductType) => (
+          <div key={products.id}>{products.title}
+
+          </div>
+        ))}
       </div>
 
     </div>
